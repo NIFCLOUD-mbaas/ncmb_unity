cd9512455b94941ae6d45f6203323234c5434dbd
ncmbsdk <ncmb_sdk@list.nifty.co.jp>
2015-03-27 17:16:59 +0900

    インデント修正、誤記修正、コメントアウト削除

--- NCMB/NCMBManager.cs
+++ NCMB/NCMBManager.cs
@@ -25,189 +25,158 @@ namespace NCMB
 	/// </summary>
 	public class NCMBManager : MonoBehaviour
 	{
-
 		//初回のみ実行フラグ
 		private static bool _isInitialized = false;
 
-        #region Const
-		const string NS = "NCMB_SPLITTER";
-        #endregion
+		#region Const
+			const string NS = "NCMB_SPLITTER";
+		#endregion
 
-        #region Static
-		internal static bool Inited { get; set; }
-        #endregion
+		#region Static
+			internal static bool Inited { get; set; }
+		#endregion
 
-        #region Delegate
-	
-		/// <summary>
-		///シーンを跨いでGameObjectを利用する
-		/// </summary>
-		public virtual void Awake ()
-		{
-			if (!NCMBManager._isInitialized) {
-				NCMBManager._isInitialized = true;
-				DontDestroyOnLoad (this.gameObject);
-			} else {
-				Destroy (this.gameObject);
-			}
-		}
-
-		/// <summary> 端末登録後のイベントリスナーです。</summary>
-		public delegate void OnRegistrationDelegate (string errorMessage);
-		/// <summary> プッシュ送信後のイベントリスナーです。</summary>
-		public delegate void OnSendPushDelegate (string errorMessage);
-		/// <summary> メッセージ受信後のイベントリスナーです。</summary>
-		public delegate void OnNotificationReceivedDelegate (NCMBPushPayload payload);
-		// <summary> 位置情報成功。</summary>
-		//public delegate void OnGetLocationSucceededDelegate(NCMBGeoPoint geo);
-		// <summary> 位置情報失敗。</summary>
-		//public delegate void OnGetLocationFailedDelegate(string errorMessage);
-
-		/// <summary> 端末登録後のイベントリスナーです。</summary>
-		public static OnRegistrationDelegate onRegistration;
-		/// <summary> プッシュ送信後のイベントリスナーです。</summary>
-		public static OnSendPushDelegate onSendPush;
-		/// <summary> メッセージ受信後のイベントリスナーです。</summary>
-		public static OnNotificationReceivedDelegate onNotificationReceived;
-		// <summary> 位置情報成功。</summary>
-		//public static OnGetLocationSucceededDelegate onGetLocationSucceeded;
-		// <summary> 位置情報失敗。</summary>
-		//public static OnGetLocationFailedDelegate onGetLocationFailed;
-        #endregion
-
-        #region Messages which are sent from native
-
-		void OnRegistration (string message)
-		{
-			Inited = true;
-			
-			if (onRegistration != null) {
-				if (message == "") {
-					message = null;
+		#region Delegate
+			/// <summary>
+		/// シーンを跨いでGameObjectを利用します。
+			/// </summary>
+			public virtual void Awake ()
+			{
+				if (!NCMBManager._isInitialized) {
+					NCMBManager._isInitialized = true;
+					DontDestroyOnLoad (this.gameObject);
+				} else {
+					Destroy (this.gameObject);
 				}
-				onRegistration (message);
 			}
-		}
 
-		void OnNotificationReceived (string message)
-		{
-			if (onNotificationReceived != null) {
-				string[] s = message.Split (new string[] { NS }, System.StringSplitOptions.None);
-				NCMBPushPayload payload = new NCMBPushPayload (s [0], s [1], s [2], s [3], s [4], s [5], s [6]);
-				onNotificationReceived (payload);
-			}
-		}
+			/// <summary> 端末登録後のイベントリスナーです。</summary>
+			public delegate void OnRegistrationDelegate (string errorMessage);
+			/// <summary> プッシュ送信後のイベントリスナーです。</summary>
+			public delegate void OnSendPushDelegate (string errorMessage);
+			/// <summary> メッセージ受信後のイベントリスナーです。</summary>
+			public delegate void OnNotificationReceivedDelegate (NCMBPushPayload payload);
+			/// <summary> 端末登録後のイベントリスナーです。</summary>
+			public static OnRegistrationDelegate onRegistration;
+			/// <summary> プッシュ送信後のイベントリスナーです。</summary>
+			public static OnSendPushDelegate onSendPush;
+			/// <summary> メッセージ受信後のイベントリスナーです。</summary>
+			public static OnNotificationReceivedDelegate onNotificationReceived;
+		#endregion
 
-		void OnSendPush (string message)
-		{
-			if (onSendPush != null) {
-				if (message == "") {
-					message = null;
+		#region Messages which are sent from native
+			void OnRegistration (string message)
+			{
+				Inited = true;
+				
+				if (onRegistration != null) {
+					if (message == "") {
+						message = null;
+					}
+					onRegistration (message);
 				}
-				onSendPush (message);
-			}
-		}
-
-		/*
-        void OnGetLocationSucceeded(string message)
-        {
-            if (onGetLocationSucceeded != null)
-            {
-                string[] s = message.Split(' ');
-                NCMBGeoPoint geo = new NCMBGeoPoint(double.Parse(s[0]), double.Parse(s[1]));
-                onGetLocationSucceeded(geo);
-            }
-        }
-
-        void OnGetLocationFailed(string message)
-        {
-            if (onGetLocationFailed != null)
-            {
-                onGetLocationFailed(message);
-            }
-        }
-         */
-        #endregion
- 
-        #region Process notification for iOS only
-        #if UNITY_IOS
-		void Start ()
-		{
-			ClearAfterOneFrame ();
-		}
-
-		void Update ()
-		{
-			if (NotificationServices.remoteNotificationCount > 0) {
-				ProcessNotification ();
-				NCMBPush.ClearAll ();
-			}
-		}
-
-		void ProcessNotification ()
-		{
-			// Payload data dictionary
-			IDictionary dd = NotificationServices.remoteNotifications [0].userInfo;
-
-			// Payload key list
-			string[] kl = new string[] { 
-                "com.nifty.PushId",
-                "com.nifty.Data",
-                "com.nifty.Title",
-                "com.nifty.Message",
-                "com.nifty.Channel",
-                "com.nifty.Dialog",
-                "com.nifty.RichUrl",
-            };
-
-			// Payload value list
-			string[] vl = new string[kl.Length];
-
-			// Index of com.nifty.Message
-			int im = 0;
-
-			// Loop list
-			for (int i = 0; i < kl.Length; i++) {
-				// Get value by key, return empty string if not exist
-				vl [i] = (dd.Contains (kl [i])) ? dd [kl [i]].ToString () : string.Empty;
-
-				// Find index of com.nifty.message
-				im = (kl [i] == "com.nifty.Message") ? i : im;
 			}
 
-			// Set message as alertBody
-			if (string.IsNullOrEmpty (vl [im])) {
-				vl [im] = NotificationServices.remoteNotifications [0].alertBody;
+			void OnNotificationReceived (string message)
+			{
+				if (onNotificationReceived != null) {
+					string[] s = message.Split (new string[] { NS }, System.StringSplitOptions.None);
+					NCMBPushPayload payload = new NCMBPushPayload (s [0], s [1], s [2], s [3], s [4], s [5], s [6]);
+					onNotificationReceived (payload);
+				}
 			}
 
-			// Create payload
-			NCMBPushPayload pl = new NCMBPushPayload (vl [0], vl [1], vl [2], vl [3], vl [4], vl [5], vl [6], NotificationServices.remoteNotifications [0].userInfo);
-
-			// Notify
-			if (onNotificationReceived != null) {
-				onNotificationReceived (pl);
+			void OnSendPush (string message)
+			{
+				if (onSendPush != null) {
+					if (message == "") {
+						message = null;
+					}
+					onSendPush (message);
+				}
 			}
-		}
+		#endregion
 
-		void OnApplicationPause (bool pause)
-		{
-			if (!pause) {
+		#region Process notification for iOS only
+
+		#if UNITY_IOS
+			void Start ()
+			{
 				ClearAfterOneFrame ();
 			}
-		}
 
-		void ClearAfterOneFrame ()
-		{
-			StartCoroutine (IEClearAfterAFrame ());
-		}
+			void Update ()
+			{
+				if (NotificationServices.remoteNotificationCount > 0) {
+					ProcessNotification ();
+					NCMBPush.ClearAll ();
+				}
+			}
 
-		IEnumerator IEClearAfterAFrame ()
-		{
-			yield return 0;
-			NCMBPush.ClearAll ();
-		}
-        #endif
-        #endregion
+			void ProcessNotification ()
+			{
+				// Payload data dictionary
+				IDictionary dd = NotificationServices.remoteNotifications [0].userInfo;
+
+				// Payload key list
+				string[] kl = new string[] { 
+					"com.nifty.PushId",
+					"com.nifty.Data",
+					"com.nifty.Title",
+					"com.nifty.Message",
+					"com.nifty.Channel",
+					"com.nifty.Dialog",
+					"com.nifty.RichUrl",
+				};
+
+				// Payload value list
+				string[] vl = new string[kl.Length];
+
+				// Index of com.nifty.Message
+				int im = 0;
+
+				// Loop list
+				for (int i = 0; i < kl.Length; i++) {
+					// Get value by key, return empty string if not exist
+					vl [i] = (dd.Contains (kl [i])) ? dd [kl [i]].ToString () : string.Empty;
+
+					// Find index of com.nifty.message
+					im = (kl [i] == "com.nifty.Message") ? i : im;
+				}
+
+				// Set message as alertBody
+				if (string.IsNullOrEmpty (vl [im])) {
+					vl [im] = NotificationServices.remoteNotifications [0].alertBody;
+				}
+
+				// Create payload
+				NCMBPushPayload pl = new NCMBPushPayload (vl [0], vl [1], vl [2], vl [3], vl [4], vl [5], vl [6], NotificationServices.remoteNotifications [0].userInfo);
+
+				// Notify
+				if (onNotificationReceived != null) {
+					onNotificationReceived (pl);
+				}
+			}
+
+			void OnApplicationPause (bool pause)
+			{
+				if (!pause) {
+					ClearAfterOneFrame ();
+				}
+			}
+
+			void ClearAfterOneFrame ()
+			{
+				StartCoroutine (IEClearAfterAFrame ());
+			}
+
+			IEnumerator IEClearAfterAFrame ()
+			{
+				yield return 0;
+				NCMBPush.ClearAll ();
+			}
+		#endif
+		#endregion
 	}
 
 	/// <summary>
@@ -229,7 +198,6 @@ namespace NCMB
 		//public bool Dialog { get; protected set; }
 		// <summary> リッチプッシュURL。</summary>
 		//public string RichUrl { get; protected set; }
-
 		/// <summary>
 		/// ペイロードのユーザー情報 (iOSのみ)
 		/// </summary>
@@ -247,6 +215,5 @@ namespace NCMB
 			//RichUrl = richUrl;
 			//UserInfo = userInfo;
 		}
-			
 	}
 }
