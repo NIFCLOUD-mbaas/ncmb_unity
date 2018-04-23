3dfa12df50a6b55aea0d86368074fad9b3d674cc
TORISOUP <tori.birdstrike@gmail.com>
2018-04-24 00:29:44 +0900

    add NCMBObjectTaskExtension

---
+++ ncmb_unity/Assets/NCMB/Script/Task/NCMBObjectTaskExtension.cs
@@ -0,0 +1,60 @@
+﻿#if (NET_4_6)
+using System.Threading.Tasks;
+
+namespace NCMB.Task
+{
+    public static class NCMBObjectTaskExtension
+    {
+        public static Task<T> FetchTaskAsync<T>(this T origin) where T : NCMBObject
+        {
+            var tcs = new TaskCompletionSource<T>();
+            origin.FetchAsync(error =>
+            {
+                if (error != null)
+                {
+                    tcs.SetException(error);
+                }
+                else
+                {
+                    tcs.SetResult(origin);
+                }
+            });
+            return tcs.Task;
+        }
+
+        public static Task<T> SaveTaskAsync<T>(this T origin) where T : NCMBObject
+        {
+            var tcs = new TaskCompletionSource<T>();
+            origin.SaveAsync(error =>
+            {
+                if (error != null)
+                {
+                    tcs.SetException(error);
+                }
+                else
+                {
+                    tcs.SetResult(origin);
+                }
+            });
+            return tcs.Task;
+        }
+
+        public static Task<T> DeleteTaskAsync<T>(this T origin) where T : NCMBObject
+        {
+            var tcs = new TaskCompletionSource<T>();
+            origin.DeleteAsync(error =>
+            {
+                if (error != null)
+                {
+                    tcs.SetException(error);
+                }
+                else
+                {
+                    tcs.SetResult(origin);
+                }
+            });
+            return tcs.Task;
+        }
+    }
+}
+#endif
---
+++ ncmb_unity/Assets/PlayModeTest/Task/NCMBTaskTestHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+
+public static class NCMBTaskTestHelper
+{
+    public static IEnumerator ToEnumerator(this Task task)
+    {
+        while (!task.IsCompleted)
+        {
+            yield return null;
+        }
+    }
+
+    public static IEnumerator ToEnumerator<T>(this Task<T> task)
+    {
+        while (!task.IsCompleted)
+        {
+            yield return null;
+        }
+    }
+}
+
