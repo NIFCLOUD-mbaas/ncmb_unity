e24443de29d8eefc085dc5a1e7d569a351c5d48e
TORISOUP <tori.birdstrike@gmail.com>
2018-04-24 00:41:34 +0900

    Add Push and Script Extensions

---
+++ ncmb_unity/Assets/NCMB/Script/Task/NCMBPushTaskExtension.cs
@@ -0,0 +1,26 @@
+﻿#if (NET_4_6)
+using System.Threading.Tasks;
+
+namespace NCMB.Task
+{
+    public static class NCMBPushTaskExtension
+    {
+        public static Task<NCMBPush> SendPushTaskAsync(this NCMBPush origin)
+        {
+            var tcs = new TaskCompletionSource<NCMBPush>();
+            origin.SendPush(error =>
+            {
+                if (error != null)
+                {
+                    tcs.SetException(error);
+                }
+                else
+                {
+                    tcs.SetResult(origin);
+                }
+            });
+            return tcs.Task;
+        }
+    }
+}
+#endif
---
+++ ncmb_unity/Assets/NCMB/Script/Task/NCMBScriptTaskExtension.cs
@@ -0,0 +1,28 @@
+﻿#if (NET_4_6)
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NCMB.Task
+{
+    public static class NCMBScriptTaskExtension
+    {
+        public static Task<byte[]> ExecuteTaskAsync(this NCMBScript script,
+            IDictionary<string, object> header, IDictionary<string, object> body, IDictionary<string, object> query)
+        {
+            var tcs = new TaskCompletionSource<byte[]>();
+            script.ExecuteAsync(header, body, query, (data, error) =>
+            {
+                if (error != null)
+                {
+                    tcs.SetException(error);
+                }
+                else
+                {
+                    tcs.SetResult(data);
+                }
+            });
+            return tcs.Task;
+        }
+    }
+}
+#endif
