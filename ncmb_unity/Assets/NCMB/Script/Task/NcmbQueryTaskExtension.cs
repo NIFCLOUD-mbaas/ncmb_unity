3185a73934fc8779aa8a05ee45dce489761a092d
TORISOUP <tori.birdstrike@gmail.com>
2018-04-24 00:38:03 +0900

    add NcmbQueryTaskExtension.cs

---
+++ ncmb_unity/Assets/NCMB/Script/Task/NcmbQueryTaskExtension.cs
@@ -0,0 +1,74 @@
+﻿#if (NET_4_6)
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NCMB.Task
+{
+    public static class NcmbQueryTaskExtension
+    {
+        /// <summary>
+        /// クエリにマッチするオブジェクトを取得を行います。
+        /// </summary>
+        public static Task<IList<T>> FindTaskAsync<T>(this NCMBQuery<T> query) where T : NCMBObject
+        {
+            var tcs = new TaskCompletionSource<IList<T>>();
+            query.FindAsync((objects, error) =>
+            {
+                if (error != null)
+                {
+                    tcs.SetException(error);
+                }
+                else
+                {
+                    tcs.SetResult(objects);
+                }
+            });
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// 指定IDのオブジェクトを取得を行います。
+        /// </summary>
+        /// <returns>結果</returns>
+        public static Task<T> GetTaskAsync<T>(this NCMBQuery<T> query, string objectId) where T : NCMBObject
+        {
+            var tcs = new TaskCompletionSource<T>();
+            query.GetAsync(objectId, (obj, error) =>
+                {
+
+                    if (error != null)
+                    {
+                        tcs.SetException(error);
+                    }
+                    else
+                    {
+                        tcs.SetResult(obj);
+                    }
+
+                });
+            return tcs.Task;
+        }
+
+        /// <summary>
+        ///クエリにマッチするオブジェクト数の取得を行います。
+        /// </summary>
+        /// <returns>カウント数</returns>
+        public static Task<int> CountTaskAsync<T>(this NCMBQuery<T> query) where T : NCMBObject
+        {
+            var tcs = new TaskCompletionSource<int>();
+            query.CountAsync((count, error) =>
+            {
+                if (error != null)
+                {
+                    tcs.SetException(error);
+                }
+                else
+                {
+                    tcs.SetResult(count);
+                }
+            });
+            return tcs.Task;
+        }
+    }
+}
+#endif
--- ncmb_unity/Assets/PlayModeTest/Task/NCMBObjectWithTaskTest.cs
+++ ncmb_unity/Assets/PlayModeTest/Task/NCMBObjectWithTaskTest.cs
@@ -1,9 +1,7 @@
 ﻿using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
-using System.Collections.Generic;
 using NCMB;
-using System.Reflection;
 using System.Threading.Tasks;
 using NCMB.Task;
 
@@ -36,35 +34,10 @@ public class NCMBObjectWithTaskTest
         // テストデータ検索
         NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("TestClass");
         query.WhereEqualTo("objectId", obj.ObjectId);
-        query.FindAsync((List<NCMBObject> list, NCMBException e) =>
-        {
-            if (e == null)
-            {
-                Assert.AreEqual("\"test\"", list[0]["key"]);
-            }
-            else
-            {
-                Assert.Fail(e.ErrorMessage);
-            }
-            NCMBTestSettings.CallbackFlag = true;
-        });
+
+        var list = await query.FindTaskAsync();
+
+        Assert.AreEqual("\"test\"", list[0]["key"]);
     }
-
-
-    /**
-     * - 内容：_getBaseUrlが返すURLが正しいことを確認する
-     * - 結果：返り値のURLが正しく取得できる事
-     */
-    [Test]
-    public void GetBaseUrlTest()
-    {
-        // テストデータ作成
-        NCMBObject obj = new NCMBObject("TestClass");
-        // internal methodの呼び出し
-        MethodInfo method = obj.GetType().GetMethod("_getBaseUrl", BindingFlags.NonPublic | BindingFlags.Instance);
-        Assert.AreEqual("http://localhost:3000/2013-09-01/classes/TestClass", method.Invoke(obj, null).ToString());
-    }
-
-
 
 }
