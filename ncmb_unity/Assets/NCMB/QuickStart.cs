3a667e70b1dd4fdec467cde7168cbfd946e8c61c
kobayashi-masaya <kobayashi.ma-91@fujitsu.com>
2020-03-26 18:06:45 +0900

    add QuickStart.cs

---
+++ ncmb_unity/Assets/NCMB/QuickStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+
+public class QuickStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // クラスのNCMBObjectを作成
+        NCMBObject testClass = new NCMBObject("TestClass");
+
+        // オブジェクトに値を設定
+
+        testClass["message"] = "Hello, NCMB!";
+        // データストアへの登録
+        testClass.SaveAsync();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
