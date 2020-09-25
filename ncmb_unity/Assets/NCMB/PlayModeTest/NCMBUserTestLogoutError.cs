edd916e951aae2aacd298fec215f0e2a3b78ebf8
vfa-tamhh <vfa.tamhh@gmail.com>
2020-09-25 15:44:24 +0700

    issue #191: fix indent

--- ncmb_unity/Assets/NCMB/PlayModeTest/NCMBUserTestLogoutError.cs
+++ ncmb_unity/Assets/NCMB/PlayModeTest/NCMBUserTestLogoutError.cs
@@ -9,14 +9,16 @@ public class NCMBUserTestLogoutError
 	[SetUp]
 	public void Init ()
 	{
-        NCMBTestSettings.Initialize ("NCMB/PlayModeTest/mbaasErrors.yaml");
+		NCMBTestSettings.Initialize ("NCMB/PlayModeTest/mbaasErrors.yaml");
 	}
+
 	[TearDown]
 	public void Teardown()
-    {
+	{
 		// Reset default mock server
 		MockServer.startMock();
 	}
+
 	/**
 	* - 内容：LogoutAsyncError
 	*/
