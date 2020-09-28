f7fe6c1db69aa9e7ffac46a30a763498f94724e0
kobayashi-masaya <kobayashi.ma-91@fujitsu.com>
2020-09-28 14:33:45 +0900

    feature add prefab and scene

---
+++ ncmb_unity/Assets/Scenes/Member/Logout.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using NCMB;
+
+public class Logout : MonoBehaviour
+{
+	NCMBUser currentUser;
+	// Use this for initialization
+	void Start ()
+	{
+		currentUser = NCMBUser.CurrentUser;
+		if (currentUser != null) {
+			// ログイン中のユーザーの取得に成功
+			UnityEngine.Debug.Log ("ログイン中のユーザー: " + currentUser.UserName);
+		} else {
+			// 未ログインまたは取得に失敗
+			UnityEngine.Debug.Log ("未ログインまたは取得に失敗");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void Logout_user ()
+	{
+		NCMBUser.LogOutAsync ((NCMBException e) => { 
+			if (e != null) {
+				UnityEngine.Debug.Log ("ログアウトに失敗: " + e.ErrorMessage);
+			} else {
+				UnityEngine.Debug.Log ("ログアウトに成功");
+				Application.LoadLevel ("Loginsignin");
+			}
+		});
+
+	}
+}
