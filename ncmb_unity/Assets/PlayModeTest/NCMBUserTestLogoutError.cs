b1e5706f5d134a883948ce305d77aff1b83ae53d
Cu Cong Can <cancc@vitalify.asia>
2021-09-06 16:08:47 +0700

    Change the project structure

---
+++ ncmb_unity/Assets/PlayModeTest/NCMBUserTestLogoutError.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.TestTools;
+using NUnit.Framework;
+using NCMB;
+using System;
+using System.Collections;
+
+public class NCMBUserTestLogoutError
+{
+	[SetUp]
+	public void Init ()
+	{
+		NCMBTestSettings.Initialize ("PlayModeTest/mbaasErrors.yaml");
+	}
+
+	[TearDown]
+	public void Teardown()
+	{
+		// Reset default mock server
+		MockServer.startMock();
+	}
+
+	/**
+	* - 内容：LogoutAsyncError
+	*/
+	[UnityTest]
+	public IEnumerator LogoutAsyncError()
+	{
+		NCMBTestSettings.CallbackFlag = false;
+		// テストデータ作成
+		NCMBUser.LogInAsync("tarou", "tarou", (e) => {
+			Assert.Null(e);
+			NCMBTestSettings.CallbackFlag = true;
+		});
+
+		yield return NCMBTestSettings.AwaitAsync();
+
+		NCMBTestSettings.CallbackFlag = false;
+		NCMBUser.LogOutAsync((e) =>
+		{
+			Assert.NotNull(e);
+			NCMBTestSettings.CallbackFlag = true;
+		});
+		yield return NCMBTestSettings.AwaitAsync();
+		Assert.True(NCMBTestSettings.CallbackFlag);
+	}
+}
