fc2935b5487f6b6a248d5215b3fa11ccb4048abf
vfa-tamhh <vfa.tamhh@gmail.com>
2021-11-05 16:39:48 +0700

    Fix indent and add permission

--- ncmb_unity/Assets/NCMB/Script/NCMBOAuthTwitter.cs
+++ ncmb_unity/Assets/NCMB/Script/NCMBOAuthTwitter.cs
@@ -6,73 +6,75 @@ namespace NCMB
 {
     public class NCMBOAuthTwitter
     {
-		private static string _consumerConsumerKey;
+        private static string _consumerConsumerKey;
         private static string _consumerSecretConsumerKey;
         private static string _callbackScheme;
-		static INCMBTwitterCallback _iNCMBTwitterCallback;
+        static INCMBTwitterCallback _iNCMBTwitterCallback;
 
 #if (UNITY_IOS && !UNITY_EDITOR && NCMB_ENABLE_TWITTER)
-		private static GameObject NCMBTwitterObject;
+        private static GameObject NCMBTwitterObject;
 #endif
 #if (UNITY_ANDROID && !UNITY_EDITOR)
         private static AndroidJavaClass androidClass;
-		private static AndroidJavaObject PluginInstance;
-        
+        private static AndroidJavaObject PluginInstance;
+
 #endif
-		public static void Init(string key, string secretKey, string scheme)
+        public static void Init(string key, string secretKey, string scheme)
         {
-			// Initial key
-			_consumerConsumerKey = key;
-			_consumerSecretConsumerKey = secretKey;
-			_callbackScheme = scheme;
+            // Initial key
+            _consumerConsumerKey = key;
+            _consumerSecretConsumerKey = secretKey;
+            _callbackScheme = scheme;
 #if (UNITY_IOS && !UNITY_EDITOR && NCMB_ENABLE_TWITTER)
-			NCMBTwitterObject = new GameObject("NCMBTwitterObject");
+            NCMBTwitterObject = new GameObject("NCMBTwitterObject");
             NCMBTwitterObject.AddComponent<TwitterComponent>();
 #endif
 #if (UNITY_ANDROID && !UNITY_EDITOR)
             AndroidJNI.AttachCurrentThread();
-			androidClass = new AndroidJavaClass("com.nifcloud.mbaas.twitterauthentication.TwitterAuthentication");
-			AndroidJavaClass playerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-			AndroidJavaObject activity = playerClass.GetStatic<AndroidJavaObject>("currentActivity");
-			androidClass.SetStatic<AndroidJavaObject>("mainActivity", activity);
-			PluginInstance = androidClass.CallStatic<AndroidJavaObject>("getInstance");
+            androidClass = new AndroidJavaClass("com.nifcloud.mbaas.twitterauthentication.TwitterAuthentication");
+            AndroidJavaClass playerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject activity = playerClass.GetStatic<AndroidJavaObject>("currentActivity");
+            androidClass.SetStatic<AndroidJavaObject>("mainActivity", activity);
+            PluginInstance = androidClass.CallStatic<AndroidJavaObject>("getInstance");
 #endif
-		}
-		public static void LogIn(INCMBTwitterCallback iNCMBTwitterCallback)
-		{
-			_iNCMBTwitterCallback = iNCMBTwitterCallback;
+        }
+        public static void LogIn(INCMBTwitterCallback iNCMBTwitterCallback)
+        {
+            _iNCMBTwitterCallback = iNCMBTwitterCallback;
 #if (UNITY_IOS) && !UNITY_EDITOR && NCMB_ENABLE_TWITTER
-			IOSTwitterLogin(
-				success => {
-					
-					_iNCMBTwitterCallback.OnSuccess(success.Id, success.Username, success.Token, success.Secret);
-				},
-				failure => {
-					_iNCMBTwitterCallback.OnFailure(failure.Message);
-				}
-				);
+            IOSTwitterLogin(
+                success =>
+                {
+
+                    _iNCMBTwitterCallback.OnSuccess(success.Id, success.Username, success.Token, success.Secret);
+                },
+                failure =>
+                {
+                    _iNCMBTwitterCallback.OnFailure(failure.Message);
+                }
+                );
 
 #endif
 #if (UNITY_ANDROID && !UNITY_EDITOR)
             AndroidTwitterLogin();
 #endif
 
-		}
+        }
 
-		private static void IOSTwitterLogin(Action<AuthToken> successCallback = null, Action<ApiError> failureCallback = null)
-		{
+        private static void IOSTwitterLogin(Action<AuthToken> successCallback = null, Action<ApiError> failureCallback = null)
+        {
 #if (UNITY_IOS) && !UNITY_EDITOR && NCMB_ENABLE_TWITTER
-			NCMBTwitterObject.GetComponent<TwitterComponent>().LoginSuccessAction = successCallback;
-			NCMBTwitterObject.GetComponent<TwitterComponent>().LoginFailureAction = failureCallback;
-			// Execute Twitter Login
-			ExecuteInvoke.NCMB_LoginWithTwitter(_consumerConsumerKey, _consumerSecretConsumerKey, _callbackScheme);
+            NCMBTwitterObject.GetComponent<TwitterComponent>().LoginSuccessAction = successCallback;
+            NCMBTwitterObject.GetComponent<TwitterComponent>().LoginFailureAction = failureCallback;
+            // Execute Twitter Login
+            ExecuteInvoke.NCMB_LoginWithTwitter(_consumerConsumerKey, _consumerSecretConsumerKey, _callbackScheme);
 #endif
-		}
+        }
 
-		private static void AndroidTwitterLogin()
+        private static void AndroidTwitterLogin()
         {
 #if (UNITY_ANDROID && !UNITY_EDITOR)
-			PluginInstance.Call("getTwitterAuthentication", _consumerConsumerKey, _consumerSecretConsumerKey, _callbackScheme, new AuThenCallback());
+            PluginInstance.Call("getTwitterAuthentication", _consumerConsumerKey, _consumerSecretConsumerKey, _callbackScheme, new AuThenCallback());
 #endif
         }
 
@@ -97,85 +99,86 @@ namespace NCMB
 #endif
 #if (UNITY_IOS) && !UNITY_EDITOR && NCMB_ENABLE_TWITTER
         private class TwitterComponent : MonoBehaviour
-		{
-			public Action<AuthToken> LoginSuccessAction { set; get; }
+        {
+            public Action<AuthToken> LoginSuccessAction { set; get; }
 
-			public Action<ApiError> LoginFailureAction { set; get; }
+            public Action<ApiError> LoginFailureAction { set; get; }
 
-			public void Awake()
-			{
-				MonoBehaviour.DontDestroyOnLoad(this);
-			}
+            public void Awake()
+            {
+                MonoBehaviour.DontDestroyOnLoad(this);
+            }
 
-			public void LoginComplete(string session)
-			{
-				if (LoginSuccessAction != null)
-				{
-					LoginSuccessAction(JsonUtility.FromJson<AuthToken>(session));
-				}
-				else
-				{
-					UnityEngine.Debug.Log("FAILED calling login success action");
-				}
-			}
+            public void LoginComplete(string session)
+            {
+                if (LoginSuccessAction != null)
+                {
+                    LoginSuccessAction(JsonUtility.FromJson<AuthToken>(session));
+                }
+                else
+                {
+                    UnityEngine.Debug.Log("FAILED calling login success action");
+                }
+            }
 
-			public void LoginFailed(string error)
-			{
-				if (LoginFailureAction != null)
-				{
-					LoginFailureAction(JsonUtility.FromJson<ApiError>(error));
-				}
-				else
-				{
-					Debug.Log("FAILED calling login fail action");
-				}
-			}
+            public void LoginFailed(string error)
+            {
+                if (LoginFailureAction != null)
+                {
+                    LoginFailureAction(JsonUtility.FromJson<ApiError>(error));
+                }
+                else
+                {
+                    Debug.Log("FAILED calling login fail action");
+                }
+            }
 
 
-		}
+        }
 #endif
-		/// <summary>
-		/// Model for response AuthToken
-		/// </summary>
-		[Serializable]
-		public class AuthToken
-		{
-			public string id;
-			public string username;
-			public string token;
-			public string secret;
+        /// <summary>
+        /// Model for response AuthToken
+        /// </summary>
+        [Serializable]
+        public class AuthToken
+        {
+            public string id;
+            public string username;
+            public string token;
+            public string secret;
 
-			public string Id { get { return this.id; } }
-			public string Username { get { return this.username; } }
-			public string Token { get { return this.token; } }
-			public string Secret { get { return this.secret; } }
+            public string Id { get { return this.id; } }
+            public string Username { get { return this.username; } }
+            public string Token { get { return this.token; } }
+            public string Secret { get { return this.secret; } }
 
-		}
+        }
 
-		/// <summary>
-		/// Model for error
-		/// </summary>
-		[Serializable]
-		public class ApiError
-		{
-			public int code;
-			public string message;
+        /// <summary>
+        /// Model for error
+        /// </summary>
+        [Serializable]
+        public class ApiError
+        {
+            public int code;
+            public string message;
 
-			public int Code { get { return this.code; } }
-			public string Message { get { return this.message; } }
+            public int Code { get { return this.code; } }
+            public string Message { get { return this.message; } }
 
-		}
+        }
 
 #if (UNITY_IOS) && !UNITY_EDITOR && NCMB_ENABLE_TWITTER
-		private static class ExecuteInvoke
-		{
-			[DllImport("__Internal")]
-			public static extern void NCMB_LoginWithTwitter(string consumerConsumerKey, string consumerSecretConsumerKey, string callbackScheme);
-		}
+        private static class ExecuteInvoke
+        {
+            [DllImport("__Internal")]
+            public static extern void NCMB_LoginWithTwitter(string consumerConsumerKey, string consumerSecretConsumerKey, string callbackScheme);
+        }
 #endif
-	}
+    }
 
-	public interface INCMBTwitterCallback {
+    public interface INCMBTwitterCallback
+    {
         void OnSuccess(string id, string userName, string token, string tokenSecret);
         void OnFailure(string errorMessage);
     }
