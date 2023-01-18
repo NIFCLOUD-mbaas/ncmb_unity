cfd5fedc9f8bcd74a18357a6281a04e9a3ff6bd9
tanaka-kokoro <kokoro.tanaka@jp.fujitsu.com>
2023-01-18 14:38:21 +0900

    Remove existed docs

--- docs/classNCMB_1_1NCMBFacebookParameters.js
+++
@@ -1,4 +0,0 @@
-var classNCMB_1_1NCMBFacebookParameters =
-[
-    [ "NCMBFacebookParameters", "classNCMB_1_1NCMBFacebookParameters.html#a28b385deb703761bf4487840cc9e4d5e", null ]
-];
--- docs/search/properties_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['badge_0',['Badge',['../classNCMB_1_1NCMBPush.html#a61f09746c248bdd22bef21565819d684',1,'NCMB::NCMBPush']]],
-  ['badgeincrementflag_1',['BadgeIncrementFlag',['../classNCMB_1_1NCMBPush.html#a14124c54e155f690dc14d0043f65814a',1,'NCMB::NCMBPush']]],
-  ['baseurl_2',['BaseUrl',['../classNCMB_1_1NCMBScript.html#af1c91f78e025542dd96baeb1b61f2167',1,'NCMB::NCMBScript']]]
-];
