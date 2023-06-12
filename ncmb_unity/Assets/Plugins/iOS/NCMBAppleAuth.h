57a9fe16cd8590c2dde7d86b772df203f26864f7
tanaka-kokoro <kokoro.tanaka@jp.fujitsu.com>
2023-06-12 22:42:14 +0900

    Restore NCMBAppleAuth plugins to resolve issue267

---
+++ ncmb_unity/Assets/Plugins/iOS/NCMBAppleAuth.h
@@ -0,0 +1,49 @@
+/*
+Copyright 2017-2022 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This file incorporates work covered by the following copyright and  
+  permission notice:  
+     https://github.com/lupidan/apple-signin-unity
+     Copyright (c) 2019 Daniel Lupiañez Casares
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+  and associated documentation files (the "Software"), to deal in the Software without restriction, 
+  including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+typedef void (*CallbackDelegate)(uint requestId,  const char* payload);
+
+@interface NCMBAppleAuth : NSObject
++ (instancetype) sharedManager;
+- (void) loginWithAppleId:(uint)requestId;
+@end
+
+void NCMBAppleAuth_HandlerCallback(CallbackDelegate callback);
+void NCMBAppleAuth_LoginWithAppleId(uint requestId);
+NS_ASSUME_NONNULL_END
